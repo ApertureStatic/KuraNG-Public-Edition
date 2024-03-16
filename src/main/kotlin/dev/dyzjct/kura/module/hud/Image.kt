0d4f38a2955014159417a043bf12a1f8e7535e3c
dyzjct <2320447878@qq.com>
2024-03-17 01:08:38 +0800

    Rimuru Image

--- src/main/kotlin/dev/dyzjct/kura/module/hud/Image.kt
+++ src/main/kotlin/dev/dyzjct/kura/module/hud/Image.kt
@@ -12,13 +12,16 @@ import dev.dyzjct.kura.utils.animations.Easing
 import net.minecraft.client.gui.DrawContext
 
 object Image : HUDModule(name = "Image", langName = "图片显示", category = Category.HUD, x = 150f, y = 150f) {
-    private val mode = msetting("Mode", Mode.Mahiro)
+    private val mode = msetting("Mode", Mode.Rimuru)
+    private val scale by fsetting("Scale", 1.0f, 0.0f, 2.0f)
+    private val rimuru = KuraIdentifier("textures/rimuru.png")
     private val arona = KuraIdentifier("textures/arona.png")
     private val mahiro = KuraIdentifier("textures/mahiro.png")
     private val roxy = KuraIdentifier("textures/roxy.png")
     var startTime = System.currentTimeMillis()
     override fun onRender(context: DrawContext) {
         val img = when (mode.value) {
+            Mode.Rimuru -> rimuru
             Mode.Arona -> arona
             Mode.Roxy -> roxy
             else -> mahiro
@@ -26,19 +29,19 @@ object Image : HUDModule(name = "Image", langName = "图片显示", category = C
         try {
             if (mc.currentScreen is ClickGuiScreen || mc.currentScreen is HudEditorScreen) {
                 RenderSystem.disableBlend()
-                width = 302f
-                height = 460f
+                width = 302f * scale
+                height = 460f * scale
                 val progress = Easing.IN_QUAD.inc(Easing.toDelta(startTime, UiSetting.animationLength))
                 context.drawTexture(
                     img,
-                    x.toInt(),
-                    y.toInt(),
+                    (x + 302f - (302f * progress)).toInt(),
+                    (y + 460f - (460f * progress)).toInt(),
                     0F,
                     0F,
-                    (302f * progress).toInt(),
-                    (460f * progress).toInt(),
-                    302,
-                    460
+                    (302f * progress * scale).toInt(),
+                    (460f * progress * scale).toInt(),
+                    (302 * progress * scale).toInt(),
+                    (460 * progress * scale).toInt()
                 )
                 RenderSystem.enableBlend()
             }
@@ -47,7 +50,8 @@ object Image : HUDModule(name = "Image", langName = "图片显示", category = C
         }
     }
 
+    @Suppress("UNUSED")
     enum class Mode {
-        Arona, Mahiro, Roxy
+        Rimuru, Arona, Mahiro, Roxy
     }
 }
