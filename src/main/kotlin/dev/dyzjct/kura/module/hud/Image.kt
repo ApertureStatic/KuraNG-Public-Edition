1da04d422861336f85ca3bc143326a43387b13e9
dyzjct <2320447878@qq.com>
2024-03-17 16:25:27 +0800

    Image AnimationType

--- src/main/kotlin/dev/dyzjct/kura/module/hud/Image.kt
+++ src/main/kotlin/dev/dyzjct/kura/module/hud/Image.kt
@@ -14,6 +14,7 @@ import net.minecraft.client.gui.DrawContext
 object Image : HUDModule(name = "Image", langName = "图片显示", category = Category.HUD, x = 150f, y = 150f) {
     val mode = msetting("Mode", Mode.Rimuru)
     private val scale by fsetting("Scale", 1.0f, 0.0f, 2.0f)
+    private val animation by msetting("AnimationType", AnimationType.Center)
     private val rimuru = KuraIdentifier("textures/rimuru.png")
     private val arona = KuraIdentifier("textures/arona.png")
     private val mahiro = KuraIdentifier("textures/mahiro.png")
@@ -32,10 +33,14 @@ object Image : HUDModule(name = "Image", langName = "图片显示", category = C
                 width = 302f * scale
                 height = 460f * scale
                 val progress = Easing.IN_QUAD.inc(Easing.toDelta(startTime, UiSetting.animationLength))
+                val animationX =
+                    if (animation == AnimationType.Center) x + ((302f - (302f * progress)) / 2) else if (animation == AnimationType.TopLeft || animation == AnimationType.DownLeft) x else x + (302f - (302f * progress))
+                val animationY =
+                    if (animation == AnimationType.Center) y + ((460f - (460f * progress)) / 2) else if (animation == AnimationType.TopLeft || animation == AnimationType.TopRight) y else y + (460f - (460f * progress))
                 context.drawTexture(
                     img,
-                    (x + 302f - (302f * progress)).toInt(),
-                    (y + 460f - (460f * progress)).toInt(),
+                    animationX.toInt(),
+                    animationY.toInt(),
                     0F,
                     0F,
                     (302f * progress * scale).toInt(),
@@ -53,5 +58,10 @@ object Image : HUDModule(name = "Image", langName = "图片显示", category = C
     @Suppress("UNUSED")
     enum class Mode {
         Rimuru, Arona, Mahiro, Roxy
+    }
+
+    @Suppress("UNUSED")
+    enum class AnimationType {
+        TopLeft, TopRight, DownLeft, DownRight, Center
     }
 }
