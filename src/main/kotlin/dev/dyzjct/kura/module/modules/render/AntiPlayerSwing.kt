77b5f606cff8e5947a189202b2e3103f1333b528
dyzjct <2320447878@qq.com>
2024-04-06 16:34:42 +0800

    FakeSneak(bug)
    cape model bug

--- src/main/kotlin/dev/dyzjct/kura/module/modules/render/AntiPlayerSwing.kt
+++ src/main/kotlin/dev/dyzjct/kura/module/modules/render/AntiPlayerSwing.kt
@@ -1,9 +1,14 @@
 package dev.dyzjct.kura.module.modules.render
 
+import base.utils.chat.ChatUtil
 import dev.dyzjct.kura.module.Category
 import dev.dyzjct.kura.module.Module
+import net.minecraft.entity.EquipmentSlot
+
 
 object AntiPlayerSwing : Module(name = "AntiPlayerSwing", langName = "防止玩家摆动", category = Category.RENDER) {
+    val fakeSneak by bsetting("FakeSneak", false)
     val arm by bsetting("Arm", false)
     val leg by bsetting("Leg", false)
+    val debug by bsetting("Debug", false)
 }
