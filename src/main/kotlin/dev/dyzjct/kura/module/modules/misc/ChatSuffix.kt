9d5868d3a5f972b9012bb5b45c758cbb79264fe9
DreamDev <3362321322@qq.com>
2024-04-29 17:30:41 +0800

    Update ChatSuffix.kt

--- src/main/kotlin/dev/dyzjct/kura/module/modules/misc/ChatSuffix.kt
+++ src/main/kotlin/dev/dyzjct/kura/module/modules/misc/ChatSuffix.kt
@@ -5,13 +5,29 @@ import dev.dyzjct.kura.module.Category
 import dev.dyzjct.kura.module.Module
 import base.events.chat.MessageSentEvent
 import base.system.event.safeEventListener
+import dev.dyzjct.kura.module.modules.client.UiSetting.Theme
+import dev.dyzjct.kura.module.modules.client.UiSetting.theme
 
 object ChatSuffix : Module(name = "ChatSuffix", langName = "后缀", category = Category.MISC) {
+    val chat = msetting("Chat", Chat.Text)
     init {
         safeEventListener<MessageSentEvent>(true) { event ->
             if (isEnabled) {
-                event.message += " Φ kนrค"
+                when (chat.value) {
+                    Chat.Kura -> {
+                        event.message += " Φ kนrค"
+                    }
+                    Chat.Icon -> {
+                        event.message += " ⚜ⓀⓊⓇⒶ⚜"
+                    }
+                    Chat.Text -> {
+                        event.message += " | \uD835\uDD76\uD835\uDD9A\uD835\uDD97\uD835\uDD86.\uD835\uDD89\uD835\uDD8A\uD835\uDD9B"
+                    }
+                }
             }
         }
     }
-}
+    enum class Chat {
+        Icon,Kura,Text
+    }
+    }
