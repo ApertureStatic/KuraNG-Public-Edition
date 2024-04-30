181d76c957ade9f60d109a45c2f18b93cc594cc2
EzDiaoL <152709512+EzDiaoL@users.noreply.github.com>
2024-04-30 18:33:24 +0800

    Update FastUse.kt

--- src/main/kotlin/dev/dyzjct/kura/module/modules/combat/FastUse.kt
+++ src/main/kotlin/dev/dyzjct/kura/module/modules/combat/FastUse.kt
@@ -11,13 +11,13 @@ object FastUse :
     Module(category = Category.COMBAT, langName = "快速使用物品", description = "Use items faster", name = "FastUse") {
     private val delay = isetting("Delay", 1, 0, 10)
     private val exp = bsetting("XP", true)
-    private val bow = bsetting("Bow", false)
+    // 弓箭没必要
+    // private val bow = bsetting("Bow", false)
     private val block = bsetting("Block", false)
 
     init {
         onMotion {
             if (player.mainHandStack.item == Items.EXPERIENCE_BOTTLE && exp.value ||
-                player.mainHandStack.item == Items.BOW && bow.value ||
                 player.mainHandStack.item is BlockItem && block.value) {
                 mc.itemUseCooldown =
                     mc.itemUseCooldown.coerceAtMost(delay.value)
