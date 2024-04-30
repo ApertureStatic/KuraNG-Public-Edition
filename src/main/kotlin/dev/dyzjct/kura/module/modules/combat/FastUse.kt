4b17997dd8e8b1ed9c13caa84ce924fd8286e1d9
EzDiaoL <152709512+EzDiaoL@users.noreply.github.com>
2024-04-30 18:26:08 +0800

    Update FastUse.kt

--- src/main/kotlin/dev/dyzjct/kura/module/modules/combat/FastUse.kt
+++ src/main/kotlin/dev/dyzjct/kura/module/modules/combat/FastUse.kt
@@ -3,6 +3,7 @@ package dev.dyzjct.kura.module.modules.combat
 import dev.dyzjct.kura.module.Category
 import dev.dyzjct.kura.module.Module
 import base.utils.concurrent.threads.runSafe
+import net.minecraft.item.BlockItem
 import net.minecraft.item.BowItem
 import net.minecraft.item.Items
 
@@ -11,13 +12,13 @@ object FastUse :
     private val delay = isetting("Delay", 1, 0, 10)
     private val exp = bsetting("XP", true)
     private val bow = bsetting("Bow", false)
+    private val block = bsetting("Block", false)
 
     init {
         onMotion {
-            if (player.mainHandStack.item == Items.EXPERIENCE_BOTTLE && exp.value) {
-                mc.itemUseCooldown =
-                    mc.itemUseCooldown.coerceAtMost(delay.value)
-            } else if (player.mainHandStack.item is BowItem && bow.value) {
+            if (player.mainHandStack.item == Items.EXPERIENCE_BOTTLE && exp.value ||
+                player.mainHandStack.item == Items.BOW && bow.value ||
+                player.mainHandStack.item is BlockItem) {
                 mc.itemUseCooldown =
                     mc.itemUseCooldown.coerceAtMost(delay.value)
             }
