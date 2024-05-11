10b9a7ad2c41e2fe3ce19a319ab9d6d5fceec8f2
dyzjct <2320447878@qq.com>
2024-05-11 22:29:13 +0800

    Update FastUse.kt

--- src/main/kotlin/dev/dyzjct/kura/module/modules/combat/FastUse.kt
+++ src/main/kotlin/dev/dyzjct/kura/module/modules/combat/FastUse.kt
@@ -8,17 +8,20 @@ import net.minecraft.item.BowItem
 import net.minecraft.item.Items
 
 object FastUse :
-    Module(category = Category.COMBAT, langName = "快速使用物品", description = "Use items faster", name = "FastUse") {
+    Module(category = Category.COMBAT, langName = "快速使用物品", description = "Use items faster", name = "FastUse", safeModule = true) {
     private val delay = isetting("Delay", 1, 0, 10)
     private val exp = bsetting("XP", true)
-    // 弓箭没必要
-    // private val bow = bsetting("Bow", false)
+
+    // 伤害箭伤害很高的
+    private val bow = bsetting("Bow", false)
     private val block = bsetting("Block", false)
 
     init {
         onMotion {
-            if (player.mainHandStack.item == Items.EXPERIENCE_BOTTLE && exp.value ||
-                player.mainHandStack.item is BlockItem && block.value) {
+            if (player.mainHandStack.item == Items.BOW && bow.value ||
+                player.mainHandStack.item == Items.EXPERIENCE_BOTTLE && exp.value ||
+                player.mainHandStack.item is BlockItem && block.value
+            ) {
                 mc.itemUseCooldown =
                     mc.itemUseCooldown.coerceAtMost(delay.value)
             }
