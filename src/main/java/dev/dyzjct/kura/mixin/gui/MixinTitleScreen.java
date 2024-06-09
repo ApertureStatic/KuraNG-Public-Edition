b9118f287380b1719c7fc84ff75296fea38914b0
dyzjct <2320447878@qq.com>
2024-06-09 23:40:12 +0800

    bug(

--- src/main/java/dev/dyzjct/kura/mixin/gui/MixinTitleScreen.java
+++ src/main/java/dev/dyzjct/kura/mixin/gui/MixinTitleScreen.java
@@ -1,24 +1,21 @@
 package dev.dyzjct.kura.mixin.gui;
 
-import base.system.render.newfont.FontRenderer;
-import base.system.render.newfont.FontRenderers;
-import dev.dyzjct.kura.Kura;
-import dev.dyzjct.kura.KuraIdentifier;
 import com.mojang.blaze3d.systems.RenderSystem;
+import dev.dyzjct.kura.KuraIdentifier;
 import dev.dyzjct.kura.module.modules.client.UiSetting;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.SplashTextRenderer;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.Util;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.awt.*;
 
 
 @Mixin(TitleScreen.class)
@@ -47,25 +44,25 @@ public abstract class MixinTitleScreen {
 
     @Inject(method = "render", at = @At("HEAD"))
     private void render(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        int width = MinecraftClient.getInstance().getWindow().getScaledWidth();
-        int height = MinecraftClient.getInstance().getWindow().getScaledHeight();
-
-        RenderSystem.enableBlend();
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.depthMask(true);
-        RenderSystem.enableDepthTest();
-
-        if (this.backgroundFadeStart == 0L && this.doBackgroundFade) {
-            this.backgroundFadeStart = Util.getMeasuringTimeMs();
-        }
-        float f = this.doBackgroundFade ? (float) (Util.getMeasuringTimeMs() - this.backgroundFadeStart) / 1000.0F : 1.0F;
-
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, f);
-
-        // 同上
-        context.drawTexture(new KuraIdentifier("background/"+ UiSetting.splashImg() + ".png"), 0, 0, 0, 0, width, height, width, height);
-
-        RenderSystem.defaultBlendFunc();
-        RenderSystem.disableBlend();
+//        int width = MinecraftClient.getInstance().getWindow().getScaledWidth();
+//        int height = MinecraftClient.getInstance().getWindow().getScaledHeight();
+//
+//        RenderSystem.enableBlend();
+//        RenderSystem.defaultBlendFunc();
+//        RenderSystem.depthMask(true);
+//        RenderSystem.enableDepthTest();
+//
+//        if (this.backgroundFadeStart == 0L && this.doBackgroundFade) {
+//            this.backgroundFadeStart = Util.getMeasuringTimeMs();
+//        }
+//        float f = this.doBackgroundFade ? (float) (Util.getMeasuringTimeMs() - this.backgroundFadeStart) / 1000.0F : 1.0F;
+//
+//        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, f);
+//
+//        // 同上
+//        context.drawTexture(new KuraIdentifier("background/" + UiSetting.splashImg() + ".png"), 0, 0, 0, 0, width, height, width, height);
+//
+//        RenderSystem.defaultBlendFunc();
+//        RenderSystem.disableBlend();
     }
 }
