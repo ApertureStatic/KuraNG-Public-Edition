43f748e809c7511b57db84bc091690243a1e97d9
dyzjct <2320447878@qq.com>
2024-06-10 12:48:22 +0800

    Update MixinPlayerListEntry.java

--- src/main/java/dev/dyzjct/kura/mixin/player/MixinPlayerListEntry.java
+++ src/main/java/dev/dyzjct/kura/mixin/player/MixinPlayerListEntry.java
@@ -1,11 +1,8 @@
 package dev.dyzjct.kura.mixin.player;
 
 import dev.dyzjct.kura.module.modules.client.Cape;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.util.SkinTextures;
-import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
