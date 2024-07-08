412f3de43083d3d19bb37865b5ba3a5b85b0a840
dyzjct <2320447878@qq.com>
2024-07-08 18:14:35 +0800

    try fix KuraIdentifier

--- src/main/kotlin/dev/dyzjct/kura/KuraIdentifier.kt
+++ src/main/kotlin/dev/dyzjct/kura/KuraIdentifier.kt
@@ -3,18 +3,39 @@ package dev.dyzjct.kura
 import net.minecraft.util.Identifier
 import java.util.*
 
-class KuraIdentifier(path: String) : Identifier(Kura.MOD_NAME.lowercase(Locale.ROOT), validatePath(path)) {
+class KuraIdentifier(path: String) {
+    private val identifier: Identifier
+
+    init {
+        val namespace = Kura.MOD_NAME.lowercase(Locale.ROOT)
+        val validatedPath = validatePath(path)
+        identifier = Identifier.of(namespace, validatedPath)
+    }
+
     companion object {
         fun validatePath(path: String): String {
-            if (isValid(path)) return path
+            if (Identifier.isPathValid(path)) return path
 
             val ret = StringBuilder()
             for (c in path.lowercase(Locale.getDefault()).toCharArray()) {
-                if (isPathCharacterValid(c)) {
+                if (Identifier.isPathCharacterValid(c)) {
                     ret.append(c)
                 }
             }
             return ret.toString()
         }
+    }
+
+    // 添加一些方法来暴露 Identifier 的功能
+    fun getNamespace(): String {
+        return identifier.namespace
+    }
+
+    fun getPath(): String {
+        return identifier.path
+    }
+
+    override fun toString(): String {
+        return identifier.toString()
     }
 }
