e530c1f62f1dc1533dd143934cfbac1fe5485f54
dyzjct <2320447878@qq.com>
2024-07-31 22:19:45 +0800

    Clip in 1.12.2 servers

--- src/main/kotlin/dev/dyzjct/kura/module/modules/misc/Clip.kt
+++ src/main/kotlin/dev/dyzjct/kura/module/modules/misc/Clip.kt
@@ -1,110 +1,84 @@
 package dev.dyzjct.kura.module.modules.misc
 
-import base.utils.concurrent.threads.runSafe
 import dev.dyzjct.kura.event.eventbus.SafeClientEvent
 import dev.dyzjct.kura.module.Category
 import dev.dyzjct.kura.module.Module
-import dev.dyzjct.kura.utils.TimerUtils
-import net.minecraft.block.Blocks
+import dev.dyzjct.kura.module.modules.movement.HoleSnap
+import dev.dyzjct.kura.module.modules.movement.Speed
+import dev.dyzjct.kura.module.modules.movement.Step
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket.PositionAndOnGround
-import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Box
+import net.minecraft.util.math.MathHelper
+import net.minecraft.util.shape.VoxelShape
 import kotlin.math.floor
 
-object Clip :
-    Module("Clip", langName = "卡墙", description = "Clip in song's server.", category = Category.MISC) {
-    private val delay by isetting("Delay", 100, 0, 500)
-    private val clipIn by bsetting("ClipIn", true)
-    private val timer = TimerUtils()
-    private var cancelPacket = true
+//Thanks For asphyxia1337(Alpha432)
+object Clip : Module(
+    name = "Clip",
+    langName = "卡墙",
+    category = Category.MISC,
+    description = "Phases into blocks nearby to prevent crystal damage."
+) {
+    private val timeout = isetting("Timeout", 5, 1, 10)
+    private val collision = isetting("CollisionSize", 2, -20, 2)
+    private var packets = 0
+    override fun onDisable() {
+        packets = 0
+    }
 
-    override fun onEnable() {
-        runSafe {
-            cancelPacket = false
-            if (clipIn) {
-                val f = player.horizontalFacing
-                player.setPosition(
-                    player.x + f.offsetX * 0.5,
-                    player.y,
-                    player.z + f.offsetZ * 0.5
-                )
-                player.networkHandler.sendPacket(
-                    PositionAndOnGround(
-                        player.x,
-                        player.y,
-                        player.z,
-                        true
-                    )
-                )
-            } else {
-                player.networkHandler.sendPacket(
-                    PositionAndOnGround(
-                        player.x,
-                        player.y,
-                        player.z,
-                        true
-                    )
-                )
-                player.setPosition(
-                    roundToClosest(
-                        player.x,
-                        floor(player.x) + 0.23,
-                        floor(player.x) + 0.77
-                    ),
-                    player.y,
-                    roundToClosest(player.z, floor(player.z) + 0.23, floor(player.z) + 0.77)
-                )
-                player.networkHandler.sendPacket(
-                    PositionAndOnGround(
-                        roundToClosest(
-                            player.x,
-                            floor(player.x) + 0.23,
-                            floor(player.x) + 0.77
-                        ),
-                        player.y,
-                        roundToClosest(player.z, floor(player.z) + 0.23, floor(player.z) + 0.77),
-                        true
-                    )
-                )
-            }
-            cancelPacket = true
-        }
+    override fun getHudInfo(): String {
+        return packets.toString()
     }
 
     init {
         onMotion {
-            if (!insideBurrow()) {
+            if (Speed.isEnabled || Step.isEnabled || HoleSnap.isEnabled) {
                 disable()
+                return@onMotion
             }
-        }
-
-        onPacketSend {
-            if (cancelPacket && it.packet is PlayerMoveC2SPacket) {
-                if (!insideBurrow()) {
-                    disable()
-                    return@onPacketSend
-                }
-                if (it.packet.changesLook()) {
-                    val packetYaw = it.packet.getYaw(0f)
-                    val packetPitch = it.packet.getPitch(0f)
-                    if (timer.passedMs(delay.toLong())) {
-                        cancelPacket = false
-                        player.networkHandler.sendPacket(
-                            PlayerMoveC2SPacket.Full(
-                                player.x,
-                                player.y + 1337,
-                                player.z,
-                                packetYaw,
-                                packetPitch,
-                                false
-                            )
-                        )
-                        cancelPacket = true
-                        timer.reset()
-                    }
-                }
-                it.cancel()
+            if (hasFewerCollisions(player, collision.value)) {
+                player.setPosition(
+                    roundToClosest(player.x, floor(player.x) + 0.301, floor(player.x) + 0.699),
+                    player.y,
+                    roundToClosest(
+                        player.z, floor(player.z) + 0.301, floor(player.z) + 0.699
+                    )
+                )
+                packets = 0
+            } else if (player.age % timeout.value == 0) {
+                player.setPosition(
+                    player.x + MathHelper.clamp(
+                        roundToClosest(
+                            player.x, floor(player.x) + 0.241, floor(
+                                player.x
+                            ) + 0.759
+                        ) - player.x, -0.03, 0.03
+                    ), player.y, player.z + MathHelper.clamp(
+                        roundToClosest(
+                            player.z, floor(player.z) + 0.241, floor(player.z) + 0.759
+                        ) - player.z, -0.03, 0.03
+                    )
+                )
+                connection.sendPacket(
+                    PlayerMoveC2SPacket.PositionAndOnGround(
+                        player.x, player.y, player.z, true
+                    )
+                )
+                connection.sendPacket(
+                    PlayerMoveC2SPacket.PositionAndOnGround(
+                        roundToClosest(
+                            player.x, floor(player.x) + 0.23, floor(
+                                player.x
+                            ) + 0.77
+                        ), player.y, roundToClosest(
+                            player.z, floor(player.z) + 0.23, floor(
+                                player.z
+                            ) + 0.77
+                        ), true
+                    )
+                )
+                packets++
             }
         }
     }
@@ -112,7 +86,6 @@ object Clip :
     private fun roundToClosest(num: Double, low: Double, high: Double): Double {
         val d1 = num - low
         val d2 = high - num
-
         return if (d2 > d1) {
             low
         } else {
@@ -120,20 +93,14 @@ object Clip :
         }
     }
 
-    private fun SafeClientEvent.insideBurrow(): Boolean {
-        val playerBlockPos: BlockPos = player.blockPos
-        for (xOffset in -1..1) {
-            for (yOffset in -1..1) {
-                for (zOffset in -1..1) {
-                    val offsetPos = playerBlockPos.add(xOffset, yOffset, zOffset)
-                    if (world.getBlockState(offsetPos).block === Blocks.BEDROCK) {
-                        if (player.boundingBox.intersects(Box(offsetPos))) {
-                            return true
-                        }
-                    }
-                }
-            }
-        }
-        return false
+    fun SafeClientEvent.hasFewerCollisions(player: PlayerEntity, collisionValue: Int): Boolean {
+        // 扩展玩家的碰撞盒子
+        val expandedBox: Box = player.boundingBox.expand(0.01, 0.0, 0.01)
+
+        // 获取与扩展后的碰撞盒子相交的所有碰撞盒子
+        val collisionBoxes: List<VoxelShape> = world.getBlockCollisions(player, expandedBox).toList()
+
+        // 检查碰撞盒子的数量是否小于指定的值
+        return collisionBoxes.size < collisionValue
     }
 }
