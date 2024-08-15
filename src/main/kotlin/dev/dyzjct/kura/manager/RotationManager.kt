5592eeca950e3f80b94895709f6ac7a984be6b5e
dyzjct <2320447878@qq.com>
2024-08-15 12:23:06 +0800

    Update RotationManager.kt

--- src/main/kotlin/dev/dyzjct/kura/manager/RotationManager.kt
+++ src/main/kotlin/dev/dyzjct/kura/manager/RotationManager.kt
@@ -1,16 +1,15 @@
 package dev.dyzjct.kura.manager
 
-import dev.dyzjct.kura.event.events.PacketEvents
-import dev.dyzjct.kura.event.events.player.PlayerMotionEvent
-import dev.dyzjct.kura.event.eventbus.AlwaysListening
-import dev.dyzjct.kura.event.eventbus.safeEventListener
 import base.utils.concurrent.threads.runSafe
 import base.utils.math.toVec3dCenter
 import base.utils.math.vector.Vec2f
+import dev.dyzjct.kura.event.eventbus.AlwaysListening
+import dev.dyzjct.kura.event.eventbus.safeEventListener
+import dev.dyzjct.kura.event.events.PacketEvents
+import dev.dyzjct.kura.event.events.player.PlayerMotionEvent
 import dev.dyzjct.kura.utils.TimerUtils
 import dev.dyzjct.kura.utils.rotation.Rotation
 import dev.dyzjct.kura.utils.rotation.RotationUtils.getFixedRotationTo
-import dev.dyzjct.kura.utils.rotation.RotationUtils.getRotationTo
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Vec3d
@@ -18,7 +17,7 @@ import java.util.concurrent.CopyOnWriteArrayList
 
 object RotationManager : AlwaysListening {
     private val resetTimer = TimerUtils()
-    private var rotateYaw = 0f
+    var rotateYaw: Float? = null
     private var rotatePitch = 0f
     private var spoofMap = CopyOnWriteArrayList<Vec2f>()
     private var stop = false
@@ -26,16 +25,24 @@ object RotationManager : AlwaysListening {
     fun onInit() {
         safeEventListener<PlayerMotionEvent>(Int.MAX_VALUE) { event ->
             if (stop) return@safeEventListener
-            if (resetTimer.passed(500)) return@safeEventListener
-            event.setRotation(rotateYaw, rotatePitch)
+            if (resetTimer.passed(500)) {
+                rotateYaw = null
+                return@safeEventListener
+            }
+            rotateYaw?.let {
+                event.setRotation(it, rotatePitch)
+            }
         }
 
         safeEventListener<PacketEvents.Send>(Int.MAX_VALUE) { event ->
-            if (stop) return@safeEventListener
-            spoofMap.forEach { _ ->
+            if (stop) {
+                rotateYaw
+                return@safeEventListener
+            }
+            rotateYaw?.let {
                 when (event.packet) {
                     is PlayerMoveC2SPacket -> {
-                        event.packet.yaw = rotateYaw
+                        event.packet.yaw = it
                         event.packet.pitch = rotatePitch
                     }
                 }
